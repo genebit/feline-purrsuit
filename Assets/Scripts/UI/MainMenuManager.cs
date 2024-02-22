232f1bd7055a34cf0c471d89a465290438887e75
genebit <genebitara@gmail.com>
2024-02-22 20:45:22 +0800

    feat: finished titlescreen

--- Assets/Scripts/UI/MainMenuManager.cs
+++ Assets/Scripts/UI/MainMenuManager.cs
@@ -5,13 +5,18 @@ namespace UI
 {
     public class MainMenuManager : MonoBehaviour
     {
-        public TransitionSettings transitionSettings;
+        [SerializeField]
+        private TransitionSettings transitionSettings;
+
+        [SerializeField]
+        private Animator boatAnimation;
 
         [Range(0, 3f)]
         public float loadDelay = 0;
 
         public void StartGame()
         {
+            boatAnimation.SetBool("TakeOff", true);
             TransitionManager.Instance().Transition("Game Scene", transitionSettings, loadDelay);
         }
 
---
+++ Assets/Scripts/UI/StartAreaController.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class StartAreaController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject optionMenu;
+
+    [SerializeField]
+    private GameObject startArea;
+
+    private void Update()
+    {
+        startArea.SetActive(!optionMenu.activeSelf);
+    }
+}
