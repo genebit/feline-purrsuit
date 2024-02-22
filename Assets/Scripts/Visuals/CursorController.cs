698924cb68e2574db4679350d52c3a0f43e7e5a1
genebit <genebitara@gmail.com>
2024-02-22 18:31:04 +0800

    feat: add cursor

---
+++ Assets/Scripts/Visuals/CursorController.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class CursorController : MonoBehaviour
+{
+    [SerializeField]
+    private ParticleSystem particles;
+
+    private Vector2 mousePosition;
+
+    private void Update()
+    {
+        // Click left mouse button to turn particles on
+        // and place them at mouse position
+        if (Input.GetMouseButtonDown(0))
+        {
+            mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            particles.Play();
+            particles.transform.position = new Vector3(mousePosition.x, mousePosition.y, 0f);
+        }
+    }
+}
