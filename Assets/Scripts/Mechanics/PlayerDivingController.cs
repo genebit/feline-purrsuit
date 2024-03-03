a7711234823cd73359e19aef9564d379826c26b4
genebit <genebitara@gmail.com>
2024-03-04 00:48:11 +0800

    feat: add underwater levels

--- Assets/Scripts/Mechanics/PlayerDivingController.cs
+++ Assets/Scripts/Mechanics/PlayerDivingController.cs
@@ -1,15 +1,27 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerDivingController : MonoBehaviour
 {
-    public float moveSpeed = 5f;
-    public float floatForce = 10f;
+    [Range(0f, 20f)]
+    public float moveSpeed;
+    [Range(0f, 20f)]
+    public float floatForce;
+    public GameObject player;
+    [Range(0f, 20f)]
+    public float staminaDecrease;
 
+    public Slider staminaSlider;
+
+    public bool isBreathing;
+
+    private SpriteRenderer playerSpriteRenderer;
     private Rigidbody2D rb;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        playerSpriteRenderer = player.GetComponent<SpriteRenderer>();
     }
 
     void Update()
@@ -19,11 +31,44 @@ public class PlayerDivingController : MonoBehaviour
         Vector2 moveDirection = new Vector2(horizontalInput, 0f).normalized;
         rb.velocity = new Vector2(moveDirection.x * moveSpeed, rb.velocity.y);
 
-        // Floating control
-        if (Input.GetKey(KeyCode.Space))
+        FlipSprite(rb.velocity);
+
+        if (!isBreathing)
         {
-            rb.AddForce(Vector2.up * floatForce * Time.deltaTime, ForceMode2D.Impulse);
+            // Floating control
+            if (Input.GetKey(KeyCode.Space))
+            {
+                rb.AddForce(Vector2.up * floatForce * Time.deltaTime, ForceMode2D.Impulse);
+            }
+
+            staminaSlider.value -= Time.deltaTime * staminaDecrease;
+            staminaSlider.value = Mathf.Clamp(staminaSlider.value, 0f, 100f);
         }
+    }
+
+    private void FlipSprite(Vector2 velocity)
+    {
+        if (velocity.x > 0)
+        {
+            // flip sprite to the right
+            playerSpriteRenderer.flipX = false;
+            player.transform.rotation = Quaternion.Euler(0, 0, -40);
+        }
+        else if (velocity.x < 0)
+        {
+            // flip sprite to the left
+            playerSpriteRenderer.flipX = true;
+            player.transform.rotation = Quaternion.Euler(0, 0, 40);
+        }
+        else
+        {
+            player.transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+    }
+
+    public void SetIsBreathing(bool value)
+    {
+        isBreathing = value;
     }
 
     private void OnTriggerEnter2D(Collider2D other)
