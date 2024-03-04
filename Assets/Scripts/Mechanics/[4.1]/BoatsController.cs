bf0c2c0891b9b15ecaa83e189372c5dff957a46c
genebit <genebitara@gmail.com>
2024-03-05 00:46:35 +0800

    feat: add boats patrolling

---
+++ Assets/Scripts/Mechanics/[4.1]/BoatsController.cs
@@ -0,0 +1,33 @@
+using Mechanics;
+using UnityEngine;
+
+public class BoatsController : MonoBehaviour
+{
+    public PatrolPath path;
+    private PatrolPath.Mover mover;
+
+    private SpriteRenderer spriteRenderer;
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    [Range(0f, 5f)]
+    public float speed;
+
+    void Update()
+    {
+        if (path != null)
+        {
+            if (mover == null) mover = path.CreateMover(speed * Time.deltaTime);
+
+            Vector2 move = mover.Position - (Vector2)transform.position;
+            move.x = Mathf.Clamp(move.x, -1, 1);
+
+            transform.position += (Vector3)move;
+
+            spriteRenderer.flipX = move.x <= 0;
+        }
+    }
+}
