49b7bf8f16c14fb833d940b673b973b939b36fe2
genebit <genebitara@gmail.com>
2024-03-06 14:30:02 +0800

    feat: add inventory to prefabs

--- Assets/Scripts/Mechanics/InventoryController.cs
+++ Assets/Scripts/Mechanics/InventoryController.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using TMPro;
+using UnityEngine;
 
 namespace Mechanics
 {
@@ -12,12 +13,37 @@ namespace Mechanics
         // rare
         public int goldFish = 0;
 
+        private int totalFishes;
+
+        public TextMeshProUGUI resourceCounter;
+
         private void Start()
         {
             // Load the inventory from the player prefs
             whiteFish = PlayerPrefs.GetInt("WhiteFish", 0);
             triangleFish = PlayerPrefs.GetInt("TriangleFish", 0);
             goldFish = PlayerPrefs.GetInt("GoldFish", 0);
+
+            totalFishes = whiteFish + triangleFish + goldFish;
+
+            // Set the resource counter
+            SetResourceCounter(totalFishes);
+        }
+
+        private void Update()
+        {
+            if (Input.GetKey(KeyCode.LeftControl) && Input.GetKeyDown(KeyCode.D))
+            {
+                Reset();
+            }
+        }
+
+        public void SetResourceCounter(int value)
+        {
+            if (resourceCounter != null)
+            {
+                resourceCounter.text = value.ToString();
+            }
         }
 
         public void AddFishToInventory(FishType fishType)
@@ -37,6 +63,22 @@ namespace Mechanics
                     PlayerPrefs.SetInt("GoldFish", goldFish);
                     break;
             }
+
+            totalFishes = whiteFish + triangleFish + goldFish;
+
+            SetResourceCounter(totalFishes);
+        }
+
+        private void Reset()
+        {
+            whiteFish = 0;
+            PlayerPrefs.DeleteKey("WhiteFish");
+            triangleFish = 0;
+            PlayerPrefs.DeleteKey("TriangleFish");
+            goldFish = 0;
+            PlayerPrefs.DeleteKey("GoldFish");
+
+            SetResourceCounter(0);
         }
     }
 }
